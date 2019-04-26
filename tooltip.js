5fb890cee976783ebe6a174955d95816bbfc14d1
Gray Smith <gray.smith00@gmail.com>
2019-04-25 21:08:10 -0600

    Initial commit

---
+++ tooltip.js
@@ -0,0 +1,30 @@
+class Tooltip extends HTMLElement {
+  constructor() {
+    super();
+    this._tooltipContainer;
+    this._tooltipText = 'Some default tooltip text.';
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+    const tooltipIcon = document.createElement('span');
+    tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
+  }
+}
+
+customElements.define('gs-tooltip', Tooltip);
